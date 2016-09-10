f37d7fbdc80f1c35a3f492332e51e9f3e52ec499
leninfernando <ingeleninfernando@gmail.com>
2016-09-10 18:17:59 -0500

    'crearutadelete'

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -33,6 +33,13 @@ def edit
 @product=Product.find(params[:id])
 end
 
+def destroy
+  product = Product.find(params[:id])
+  product.destroy
+
+  redirect_to products_path, notice: "El producto fue eliminado con éxito"
+end
+
 def update
 @product=Product.find(params[:id])
 if @product.update(product_params)
