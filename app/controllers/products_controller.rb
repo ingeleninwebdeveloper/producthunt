88773570c5ee2d5d65c097c6c1f570adfb6de8ad
leninfernando <ingeleninfernando@gmail.com>
2016-09-10 14:24:04 -0500

    'crearutapost/products'

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -14,5 +14,21 @@ def new
 end
 
 
+def create
+
+@product=Product.new(product_params)
+if @product.save
+	redirect_to products_path, notice:'el producto fue publicado con exito'
+else
+	render :new
+end
+end
+
+private
+
+def product_params
+
+	params.require(:product).permit(:name, :url, :description)
+end
 
 end
